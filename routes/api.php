143c0216e8e3db349f4bff8d0a702e161384a266
Developer <halaszgabor@hamito.eu>
2024-02-06 12:02:03 +0100

    [Dev] - Kernel, routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\AccountController;
+use App\Http\Controllers\CafeteriaController;
+use App\Http\Controllers\CalendarController;
+use App\Http\Controllers\PocketController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +21,13 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::post('/account/create',     [AccountController::class,   'create']);
+Route::post('/account/list',       [AccountController::class,   'list']);
+Route::post('/cafeteria/export',   [CafeteriaController::class, 'export']);
+Route::post('/cafeteria/read',     [CafeteriaController::class, 'read']);
+Route::post('/cafeteria/write',    [CafeteriaController::class, 'write']);
+Route::post('/calendar/available', [CalendarController::class,  'available']);
+Route::post('/calendar/create',    [CalendarController::class,  'create']);
+Route::post('/calendar/list',      [CalendarController::class,  'list']);
+Route::post('/pocket/create',      [PocketController::class,    'create']);
+Route::post('/pocket/list',        [PocketController::class,    'list']);
